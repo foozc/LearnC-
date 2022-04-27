31c59bbc7b02ece48409b6f4aa5c093d23798848
foozc <foozc@outlook.com>
2022-04-27 23:31:32 +0800

    模块，函数的新建

---
+++ C++_模块_函数/C++_模块_函数/_1_函数_函数的定义.cpp
@@ -0,0 +1,20 @@
+﻿//文件名：_1_函数_函数的定义
+//创建时间：2022/4/27 23:28:08
+
+#include<iostream>
+using namespace std;
+
+int add(int num1, int num2) {
+	return num1 + num2;
+}
+
+int main()
+//int main__1_函数_函数的定义()
+{
+	int a = 1, b = 2;
+	int c = add(a, b);
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
+	return 0;
+}
