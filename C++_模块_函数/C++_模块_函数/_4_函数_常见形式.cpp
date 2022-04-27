c6676d14ff687c18e014ec1eb02c4b54388f27bd
foozc <foozc@outlook.com>
2022-04-28 00:08:58 +0800

    函数四种常见形式，有参有饭

--- C++_模块_函数/C++_模块_函数/_4_函数_常见形式.cpp
+++ C++_模块_函数/C++_模块_函数/_4_函数_常见形式.cpp
@@ -4,9 +4,37 @@
 #include<iostream>
 using namespace std;
 
-//int main__4_函数_常见形式()
+//有参有返
+int add1(int a, int b) {
+	return a + b;
+}
+//有参无返
+void swap1(int a, int b) {
+	int temp = b;
+	b = a;
+	a = temp;
+}
 
+//无参有饭
+int num() {
+	return 1;
+}
+
+//无参午饭
+void test() {
+	cout << "helloWorld" << endl;
+}
+
+
+int main__4_函数_常见形式()
+//int main()
 {
+	int a = 1, b = 2;
+	int c = num();
+	int d = add1(a , b);
+	test();
 
+
+	
 	return 0;
 }
