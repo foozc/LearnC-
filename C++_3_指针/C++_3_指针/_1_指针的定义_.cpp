8c1c8f29c6e3657350b1435aff81362cbe28d3ad
foozc <foozc@outlook.com>
2022-05-08 00:48:38 +0800

    指针的定义

---
+++ C++_3_指针/C++_3_指针/_1_指针的定义_.cpp
@@ -0,0 +1,18 @@
+﻿//文件名：_1_指针的定义_
+//创建时间：2022/5/8 0:33:41
+
+#include<iostream>
+using namespace std;
+
+//int main__1_指针的定义_()
+int main()
+{
+	int a = 10;
+	int *p = &a;
+	cout <<"a="<< a << endl;
+	cout <<"p="<< p << endl;
+	cout <<"*p="<< *p<<endl;
+	cout << "p size=" << sizeof(p) << endl;
+	cout << "*p size=" << sizeof(*p) << endl;//指针的大小是根据声明条件来
+	return 0;
+}
